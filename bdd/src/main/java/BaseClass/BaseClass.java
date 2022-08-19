9ae007cfe6a18fac5749c23d49e2080564d6eddf
yogi0513 <yogendraburkul0513@gmail.com>
2022-08-19 13:24:31 +0530

    updated base class

--- bdd/src/main/java/BaseClass/BaseClass.java
+++ bdd/src/main/java/BaseClass/BaseClass.java
@@ -13,7 +13,7 @@ public class BaseClass {
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://www.facebook.com/");
 //		driver.get("https://classic.crmpro.com/index.html");
 	}
